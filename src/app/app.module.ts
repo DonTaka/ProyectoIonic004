43dc2492a15d6bd1bcdeba83871ef8f81f9ccd3d
dontaka <jo.riquelmees@gmail.com>
2023-08-25 15:36:10 -0400

    Update Material/Animation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,34 +1,45 @@
-import { Component,ElementRef,ViewChild} from '@angular/core';
+import { Component,ViewChild,ElementRef} from '@angular/core';
 import { Router, NavigationExtras } from '@angular/router';
-import { IonAvatar,AnimationController} from '@ionic/angular';
+import { IonAvatar } from '@ionic/angular';
 import type { Animation } from '@ionic/angular';
-
+import { AnimationController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  //ViewChild(TipoComponente,{read:ElementRef}) nombre!:ElementRef<HTMLTipoObjetoElement>
   @ViewChild(IonAvatar,{read:ElementRef}) avatar!:ElementRef<HTMLIonAvatarElement>;
- 
-  constructor(private router: Router,private animationCtrl:AnimationController) { }
+
   private animation!:Animation;
+  private animationn!:Animation;
+  constructor(private router: Router,private animationCtrl:AnimationController) { }
   public mensaje = ""
 
   ngAfterViewInit() {
     this.animation = this.animationCtrl.create()
     .addElement(this.avatar.nativeElement)
-    .duration(1500) 
-    .fromTo('transform', 'translateX(0px)', 'translateX(100px)')
-    .fromTo('opacity', '1', '0.2');
+    .duration(5000)
+    .iterations(Infinity)
+    .keyframes([
+      {offset:0, transform:'translateX(0px)',opacity:'1'},
+      {offset:0.25, transform:'translateX(100px)',opacity:'0.2'},
+      {offset:0.50, transform:'translateX(0px)',opacity:'1'},
+      {offset:0.75, transform:'translateX(-100px)',opacity:'0.2'},
+      {offset:1, transform:'translateX(0px)',opacity:'1'},
+    ])
+
+
+    
+
   }
+
   user = {
     usuario: "",
     password: ""
   }
   
-  avatarPlay(){
+  playAvatar(){
     this.animation.play();
   }
 
